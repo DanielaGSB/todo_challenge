65582c17440e169b09764e95c86bd3a81edcb1f0
Daniela <daniela.galeote@gmail.com>
2016-01-31 16:50:00 +0000

    Setting testing framework

---
+++ test/toDoListController.spec.js
@@ -0,0 +1,16 @@
+describe('ToDoListController', function() {
+  beforeEach(module('ToDoList'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ToDoListController');
+  }));
+
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+
+
+});
